f90a87fcf068fc3a446625e3abdbd3add2f8f154
Angelo Salles <angelo_848@live.com>
2020-02-15 13:56:16 -0300

    added complete task feature

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faCircle,
@@ -9,9 +9,18 @@ import {
 import { Todo } from './styles'
 
 export default function Task(props) {
+  const [status, setStatus] = useState(false)
+
+  const toggleTask = () => {
+    status ? setStatus(false) : setStatus(true)
+  }
+
   return (
     <Todo color={props.color}>
-      {/* <FontAwesomeIcon icon={status ? faCheckCircle : faCircle} /> */}
+      <FontAwesomeIcon
+        icon={status ? faCheckCircle : faCircle}
+        onClick={toggleTask}
+      />
       <div>
         {props.value}
         <FontAwesomeIcon
