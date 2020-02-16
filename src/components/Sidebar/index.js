7683badd4bb20d8e31dff8ea7180c15ea4846b12
Angelo Salles <angelo_848@live.com>
2020-02-16 11:21:40 -0300

    add sidebar stylization

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars, faHome } from '@fortawesome/free-solid-svg-icons'
+import { faCalendar, faStar } from '@fortawesome/free-regular-svg-icons'
 
 import { Container, List } from './styles'
 
@@ -16,8 +17,25 @@ export default function Sidebar() {
       <FontAwesomeIcon icon={faBars} onClick={toggleSidebar} />
       <List>
         <li>
-          <FontAwesomeIcon icon={faHome} />
-          <span>Tarefas</span>
+          <div>
+            <FontAwesomeIcon icon={faHome} />
+            <span>Tarefas</span>
+          </div>
+          <span>2</span>
+        </li>
+        <li>
+          <div>
+            <FontAwesomeIcon icon={faStar} />
+            <span>Favoritos</span>
+          </div>
+          <span>5</span>
+        </li>
+        <li>
+          <div>
+            <FontAwesomeIcon icon={faCalendar} />
+            <span>Agendado</span>
+          </div>
+          <span>4</span>
         </li>
       </List>
     </Container>
