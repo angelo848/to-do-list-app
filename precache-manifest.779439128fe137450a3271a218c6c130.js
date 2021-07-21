af0f6852fcc027fdc169a5afc8e0f6a337395bd7
Angelo Salles <angelo.salles@mercadolivre.com>
2021-07-20 23:38:27 -0300

    Updates

---
+++ precache-manifest.779439128fe137450a3271a218c6c130.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb5830ee0cc5479d1945e3a7a2933bb1",
+    "url": "/to-do-list-app/index.html"
+  },
+  {
+    "revision": "c79661e8b8dc3b340e79",
+    "url": "/to-do-list-app/static/css/main.b472403f.chunk.css"
+  },
+  {
+    "revision": "da9fb24c719a4a6ead75",
+    "url": "/to-do-list-app/static/js/2.8e517104.chunk.js"
+  },
+  {
+    "revision": "e540cb51dc4f24037f1158e0238ef6f2",
+    "url": "/to-do-list-app/static/js/2.8e517104.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c79661e8b8dc3b340e79",
+    "url": "/to-do-list-app/static/js/main.49ce7de2.chunk.js"
+  },
+  {
+    "revision": "a0bbc03d64c922076f73",
+    "url": "/to-do-list-app/static/js/runtime-main.d6f9ffe3.js"
+  }
+]);
